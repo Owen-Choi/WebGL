1a15ea3a6dae2650e4db29a0c3c4ce74e546fd01
Owen-Choi <oldstyle482@gmail.com>
2022-10-19 14:17:46 +0900

    camera movement trial

--- src/App.js
+++ src/App.js
@@ -1,19 +1,14 @@
-import React, { Suspense, useEffect, useState} from "react";
+import React, { Suspense, useRef} from "react";
 import {Canvas, useFrame} from "react-three-fiber";
 import { DoubleSide, RepeatWrapping, sRGBEncoding } from "three";
 import {
   Loader,
   OrbitControls,
+  PerspectiveCamera,
   useTexture,
 } from "@react-three/drei";
-import { PerspectiveCamera, PositionalAudio} from '@react-three/drei'
 import { vertexShader, fragmentShader } from "./shaders";
-
 import "./style.css";
-
-const [xAxis, setXAxis] = useState(0.5);
-const [yAxis, setYAxis] = useState(0.5);
-const [zAxis, setZAxis] = useState(0.5);
 
 export default function App() {
   return (
@@ -26,13 +21,21 @@ export default function App() {
           <ambientLight />
         </Suspense>
         <PerspectiveCamera
-          position={[xAxis, yAxis, zAxis]}
-          near={0.01}
-          far={1000}
-          makeDefault
+            position={[0.5, 0.5, 0.5]}
+            near={0.01}
+            far={1000}
+            makeDefault
         />
         <OrbitControls
-            screenSpacePanning={false}
+            screenSpacePanning={true}
+            enableKeys={true}
+            makeDefault enabled={false}
+            // keys={[
+            //   LEFT : 89, //left arrow
+            //   UP: 'ArrowUp', // up arrow
+            //   RIGHT: 'ArrowRight', // right arrow
+            //   BOTTOM: 'ArrowDown'
+            // ]}
         />
       </Canvas>
       <Loader />
@@ -40,7 +43,9 @@ export default function App() {
   );
 }
 
-function Terrain() {
+const Terrain = () => {
+
+  const pos = useRef();
   // Load the heightmap image
   const heightMap = useTexture("/uluru-heightmap.png");
   // Apply some properties to ensure it renders correctly
@@ -57,12 +62,24 @@ function Terrain() {
   textureMap.wrapT = RepeatWrapping;
   textureMap.anisotropy = 16;
 
+  // useFrame(() => {
+  //   pos.current.position.x += 0.000001;
+  //   pos.current.position.y += 0.000001;
+  //   pos.current.position.z += 0.000001;
+  //   console.log(pos.current.position.z);
+  // })
+
   return (
     <mesh
-      position={[-0.1, 0.3, 0]}
+      position={[0, 0, 0]}
       rotation={[-Math.PI / 2, 0, 0]}
       scale={[1 / 1024, 1 / 1024, 1 / 1024]}
     >
+      {/*<PerspectiveCamera ref={pos}*/}
+      {/*                   position={[0.5, 0.5, 0.5]}*/}
+      {/*                   near={0.01}*/}
+      {/*                   far={1000}*/}
+      {/*                   makeDefault />*/}
       <planeBufferGeometry args={[1024, 1024, 256, 256]} />
       <shaderMaterial
         uniforms={{
